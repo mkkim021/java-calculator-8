ddb2dc2f24f2064fb69be6c68906928efd5d3d37
mkkim21 <dgd5980@naver.com>
2025-10-16 21:35:32 +0900

    feat(InputParser): 커스텀 구분자 지정 및 커스텀 구분자 기준 파싱

--- src/main/java/calculator/model/InputParser.java
+++ src/main/java/calculator/model/InputParser.java
@@ -6,14 +6,39 @@ import java.util.List;
 
 public class InputParser {
     private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_POSTFIX = "\n";
 
 
     public List<Integer> parsing(String input) {
         if (input == null || input.isEmpty()) {
             return Collections.emptyList();
         }
+        String[] parsed = extractCustomDeli(input);
+        String delimiter = parsed[0];
+        String parsedInput = parsed[1];
 
-        return extractNumbers(DEFAULT_DELIMITER, input);
+        if (parsedInput == null || parsedInput.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return extractNumbers(delimiter, parsedInput);
+    }
+
+    public String[] extractCustomDeli(String input) {
+
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            String normalInput = input.replace("\\n", CUSTOM_DELIMITER_POSTFIX);
+
+            int deliPostIdx = normalInput.indexOf(CUSTOM_DELIMITER_POSTFIX);
+
+            String customDeli = normalInput.substring(2, deliPostIdx);
+            String convertInput = normalInput.substring(deliPostIdx + 1);
+            String normalDeli = "[" + customDeli + "]";
+            return new String[]{normalDeli, convertInput};
+        }
+
+        return new String[]{DEFAULT_DELIMITER, input};
     }
 
 
