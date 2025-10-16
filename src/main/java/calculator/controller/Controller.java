9018dc6b062718c3177d4eb6dec2a9d7adcf87e0
mkkim21 <dgd5980@naver.com>
2025-10-16 16:44:11 +0900

    feat(Controller):계산기 기본 흐름 구성

---
+++ src/main/java/calculator/controller/Controller.java
@@ -0,0 +1,27 @@
+package calculator.controller;
+
+import calculator.view.InputView;
+
+public class Controller {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Calculator calculator;
+
+    public Controller(InputView inputView, OutputView outputView, Calculator calculator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.calculator = calculator;
+    }
+
+    public void run() {
+        try {
+            String input = inputView.getInput();
+            int result = calculator.calculate(input);
+            outputView.printResult(result);
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
